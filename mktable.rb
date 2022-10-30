65720dc98fe7a14a49d95d1822e0a6ca5390ba5f
Carsten Bormann <cabo@tzi.org>
2022-10-30 20:59:39 +0100

    More real tables

--- mktable.rb
+++ mktable.rb
@@ -6,9 +6,9 @@ def sz(fn)
   File.stat(fn).size
 end
 
-puts "%20s | json | cbor | half | red." % "example"
+puts "| %20s | json | cbor | half | red." % "example"
 files.each do |f|
-  puts "%20s | %4d | %4d | %4d | %d %%" % [f,
+  puts "| %20s | %4d | %4d | %4d | %d %%" % [f,
        j = JSON.generate(JSON.parse(File.read("json/#{f}.json"))).size,
        c = sz("cbor/#{f}.cbor"),
        sz("cbor-half/#{f}.cbor"),
